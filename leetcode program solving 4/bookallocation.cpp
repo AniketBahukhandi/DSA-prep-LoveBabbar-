b346236a2af7459e103e520f68482ca19fe38143
aniket bahukhandi <aniketbahukhandi@gmail.com>
2022-05-05 23:58:54 +0530

    initial commit

--- leetcode program solving 4/bookallocation.cpp
+++ leetcode program solving 4/bookallocation.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
 using namespace std;
-int main()
-{
+void userin(int arr[],int size){
+for(int i=0;i<size;i++){
+cin>>arr[i];
+}
+}
+void printa(int arr[],int size){
+ for(int i=0;i<size;i++){
+cout<<arr[i]<<"";
+}
+cout<<endl;
+}
+int bookallocation(int arr[],int n){
 
+}
+int main()
+{   int n;
+    cin>>n;
+    int arr[100];
+    userin(arr,n);
+    cout<<bookallocation(arr,n);
     return 0;
 }
