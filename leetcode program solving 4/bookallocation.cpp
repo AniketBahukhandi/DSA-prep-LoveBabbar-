b0cde898d4bb5ee6d21718d90296b1776059c1b1
aniket bahukhandi <aniketbahukhandi@gmail.com>
2022-05-06 23:26:22 +0530

    initial commir

--- leetcode program solving 4/bookallocation.cpp
+++ leetcode program solving 4/bookallocation.cpp
@@ -11,14 +11,54 @@ cout<<arr[i]<<"";
 }
 cout<<endl;
 }
-int bookallocation(int arr[],int n){
+int bookallocation(int arr[],int n,int s){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+    }
+    int s=0;
+    int e=sum;
+    int ans=-1;
+    int mid=s+(e-s)/2;
+    while(s<=e){
+        if(ispossible(arr,n,s,mid)){
+            ans=mid;
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+        mid=s+(e-s)/2;
+    }
+return ans;
+    
+}
+bool ispossible(int arr[],int n,int s,int mid){
+    int pagesum=0;
+    int studentcount=1;
+    for(int i=0;i<n;i++){
+        if(pagesum+arr[i]<=mid){
+            pagesum+=arr[i];
+        }
+        else{
+            studentcount+=1;
+            if(studentcount>s ||arr[i]>mid){
 
+            }
+            pagesum=arr[i];
+        }
+        
+    }
+    return true;
 }
 int main()
 {   int n;
     cin>>n;
     int arr[100];
     userin(arr,n);
-    cout<<bookallocation(arr,n);
+    cout<<"enter the no of students";
+    int s;
+    cin>>s;
+    cout<<bookallocation(arr,n,s);
     return 0;
 }
