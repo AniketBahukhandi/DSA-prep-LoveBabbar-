c2949689c404e69227c735445df3396581c7196a
aniket bahukhandi <aniketbahukhandi@gmail.com>
2022-05-20 23:56:04 +0530

    insertion sort

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -1,9 +1,38 @@
 #include<iostream>
 using namespace std;
+void userin(int arr[],int size){
+for(int i=0;i<size;i++){
+cin>>arr[i];
+}
+}
+void printa(int arr[],int size){
+ for(int i=0;i<size;i++){
+cout<<arr[i]<<" ";
+}
+cout<<endl;
+}
+void insertionsort(int arr[],int size){
+    for(int i=1;i<size;i++){
+        int temp=arr[i];
+        int j=i-1;
+        for(;j>=0;j--){
+            if(arr[j]>temp){
+                arr[j+1]=arr[j];
+            }
+            else{
+                break;
+            }
+        }
+        arr[j+1]=temp;
+    }
+}
 int main()
 {
     int n;
     cin>>n;
-    
+    int arr[n];
+    userin(arr,n);
+    insertionsort(arr,n);
+    printa(arr,n);
     return 0;
 }
